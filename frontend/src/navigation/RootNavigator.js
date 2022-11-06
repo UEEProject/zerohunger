12f39dee8b35ed054c2800d511b570558d5911a5
Ishan Nadeera <shanf753@gmail.com>
2022-11-06 22:19:45 +0530

    Updated Root Navigator

--- frontend/src/navigation/RootNavigator.js
+++ frontend/src/navigation/RootNavigator.js
@@ -11,6 +11,10 @@ import CashDonationForm2Screen from '../screens/DonationFunction/CashDonationFor
 import ItemsDonationForm1Screen from '../screens/DonationFunction/ItemsDonationForm1Screen';
 import ItemsDonationForm2Screen from '../screens/DonationFunction/ItemsDonationForm2Screen';
 import ItemsDonationForm3Screen from '../screens/DonationFunction/ItemsDonationForm3Screen';
+import AddWorkshopCategory from '../screens/WorkshopFunction/AddWorkshopCategory';
+import AddWorkshop from '../screens/WorkshopFunction/AddWorkshop';
+import Guidance from '../screens/GuidanceFunction/GuidanceInitialScreen';
+
 
 
 const Stack = createNativeStackNavigator();
@@ -20,12 +24,20 @@ const RootNavigator = () => {
             <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name ="Splash" component={SplashScreen} />
                 <Stack.Screen name ="Login" component={LoginScreen} />
+
                 <Stack.Screen name ="DonationInitial" component={DonationInitialScreen} />
+
                 <Stack.Screen name ="CashDonationForm1" component={CashDonationForm1Screen} />
                 <Stack.Screen name ="CashDonationForm2" component={CashDonationForm2Screen} />
+
                 <Stack.Screen name ="ItemsDonationForm1" component={ItemsDonationForm1Screen} />
                 <Stack.Screen name ="ItemsDonationForm2" component={ItemsDonationForm2Screen} />
                 <Stack.Screen name ="ItemsDonationForm3" component={ItemsDonationForm3Screen} />
+
+                <Stack.Screen name ="AddWorkshopCategory" component={AddWorkshopCategory} />
+                <Stack.Screen name ="AddWorkshop" component={AddWorkshop} />
+
+                <Stack.Screen name ="Guidance" component={Guidance} />
             </Stack.Navigator>
         </NavigationContainer>
     );
